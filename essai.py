cd875db3865c15cc349aab1675b3fba67091b230
Bruno Ricci <brricci@proton.me>
2025-03-15 16:23:53 +0100

    second commit

--- essai.py
+++ essai.py
@@ -4,10 +4,24 @@ def get_full_name(first_name: str, last_name: str):
     full_name = first_name.title() + " " + last_name.lower()
     return full_name
 
+def process_items0(items: list[str]):
+    for item in items:
+        print(item)
+
+def process_items1(prices: dict[str, float]):
+    for item_name, item_price in prices.items():
+        print(item_name)
+        print(item_price)
 
 def main():
     print("Hello World!")
     print( get_full_name("JOHN", "Houston"))
+    l : list[str] = ["François", "Michel", "Robert"]
+    process_items0(l)
+    d : dict[str, float] = {"first": 100, "second": 200, "third": 300}
+    process_items1(d)
+
+
 
 if __name__ == "__main__":
     main()
